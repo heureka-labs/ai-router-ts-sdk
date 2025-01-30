e3dffbd25f731d1c59fe9f0587dc0714dd104603
Matthias Lau <lau.matthias@gmail.com>
2025-01-30 12:39:29 +0100

    Adjust to python sdk, return only the model and add model class for abstraction

--- src/router.ts
+++ src/router.ts
@@ -1,44 +1,73 @@
-import type { AiRouterModel } from "./types/model"
-import type { AiRouterEmbeddingModel } from "./types/embeddingModel"
+import { AiRouterModel } from "./types/model"
+import type { AiRouterEmbeddingType } from "./types/embeddingModel"
+
+const AI_ROUTER_API_KEY_ENV_VAR_NAME = "AIROUTER_API_KEY"
+const AI_ROUTER_BASE_URL_ENV_VAR_NAME = "AIROUTER_HOST"
+const AI_ROUTER_BASE_URL = "https://api.airouter.io"
 
 type Message = { role: string; content: string }
 
 export type GetBestModelParams =
   | {
       messages: Message[]
+      fullPrivacy?: boolean
       embedding?: never
-      embeddingModel?: never
+      embeddingType?: never
     }
   | {
       messages?: never
+      fullPrivacy?: boolean
       embedding: number[]
-      embeddingModel: AiRouterEmbeddingModel
+      embeddingType: AiRouterEmbeddingType
     }
 
 export class AiRouter {
   private apiKey: string
   private baseUrl: string
-  constructor(apiKey?: string, baseUrl: string = "https://api.airouter.io") {
+  constructor(apiKey?: string, baseUrl?: string) {
+    // set the airouter api key
     if (!apiKey) {
-      apiKey = process.env.AI_ROUTER_API_KEY
+      apiKey = process.env[AI_ROUTER_API_KEY_ENV_VAR_NAME]
       if (!apiKey) {
-        throw new Error("AI_ROUTER_API_KEY is required")
+        throw new Error(
+          `${AI_ROUTER_API_KEY_ENV_VAR_NAME} must be set either by passing apiKey to the constructor or by setting the ${AI_ROUTER_API_KEY_ENV_VAR_NAME} environment variable`,
+        )
       }
     }
     this.apiKey = apiKey
-    this.baseUrl = baseUrl
+
+    // adjust the base url to the airouter
+    this.baseUrl =
+      baseUrl ??
+      process.env[AI_ROUTER_BASE_URL_ENV_VAR_NAME] ??
+      AI_ROUTER_BASE_URL
+
+    console.log("AI Router initialized with base URL:", this.baseUrl)
   }
 
   public async getBestModel({
     messages,
+    fullPrivacy = false,
     embedding,
-    embeddingModel,
+    embeddingType,
   }: GetBestModelParams): Promise<AiRouterModel> {
-    if (messages) {
-      return await this.getBestModelByMessages(messages)
-    } else {
-      return await this.getBestModelByEmbedding(embedding, embeddingModel)
+    // explicitly set full privacy if an embedding is provided
+    if (embedding) {
+      fullPrivacy = true
     }
+
+    // in full privacy mode, send embeddings instead of messages
+    if (fullPrivacy) {
+      if (!embedding) {
+        throw new Error(
+          `Automatically generating embeddings for the full privacy mode is not supported yet. Please provide an embedding.`,
+        )
+      }
+      return await this.getBestModelByEmbedding(embedding, embeddingType)
+    }
+
+    // in model selection mode, send messages to the airouter to receive the best model
+    return await this.getBestModelByMessages(messages)
   }
 
   private async getBestModelByMessages(
@@ -49,19 +78,22 @@ export class AiRouter {
 
   private async getBestModelByEmbedding(
     embedding: number[],
-    embeddingModel: AiRouterEmbeddingModel,
+    embeddingType: AiRouterEmbeddingType,
   ): Promise<AiRouterModel> {
-    return await this.executeRequest({ embedding, embeddingModel })
+    return await this.executeRequest({
+      embedding,
+      embeddingType,
+    })
   }
 
   private async executeRequest({
     messages,
     embedding,
-    embeddingModel,
+    embeddingType,
   }: GetBestModelParams): Promise<AiRouterModel> {
     const body = messages
       ? { messages }
-      : { embedding, embedding_type: embeddingModel }
+      : { embedding, embedding_type: embeddingType }
     const response = await fetch(`${this.baseUrl}/v1/chat/completions`, {
       method: "POST",
       headers: {
@@ -76,10 +108,16 @@ export class AiRouter {
     })
 
     if (!response.ok) {
-      console.error(response.json())
-      throw new Error("Failed to run ai router request")
+      const errorData = await response.json()
+      console.error(errorData)
+      throw new Error("Failed to run AI Router request")
     }
 
-    return await response.json()
+    const responseJson = await response.json()
+    const bestModel = AiRouterModel.fromString(
+      responseJson.choices[0].message.content,
+    )
+
+    return bestModel
   }
 }
